c3f97ada52d95aec86b57878f780ff822468271e
Firman, Max <max.firman@troweprice.com>
2023-03-16 17:37:28 +0000

    Add some logging and update BUCKET name

--- dbt_server/s3.py
+++ dbt_server/s3.py
@@ -2,12 +2,13 @@ import os
 
 import s3fs
 
-from dbt_server.services import filesystem_service
+from dbt_server.logging import DBT_SERVER_LOGGER as logger
 
-BUCKET = "dbt-server"
+BUCKET = f'dp-all-trp-{os.environ["TRP_AWS_ENVIRONMENT"].lower()}-dbt-server'
 
 
 def create_filesystem() -> s3fs.S3FileSystem:
+    logger.info("Creating S3FileSystem")
     endpoint_url = os.getenv("AWS_S3_ENDPOINT_URL")
     if endpoint_url is None:
         return s3fs.S3FileSystem()
@@ -17,9 +18,13 @@ def create_filesystem() -> s3fs.S3FileSystem:
 
 def put_cache() -> None:
     fs = create_filesystem()
-    fs.put(filesystem_service.DEFAULT_WORKING_DIR, f"/{BUCKET}/", recursive=True)
+    logger.info("Uploading project state to s3 cache")
+    if fs.exists(f"/{BUCKET}/working-dir/"):
+        fs.rm(f"/{BUCKET}/working-dir/", recursive=True)
+    fs.put("./working-dir", f"/{BUCKET}/", recursive=True)
 
 
 def get_cache() -> None:
     fs = create_filesystem()
+    logger.info("Retrieving project state from s3 cache")
     fs.get(f"s3://{BUCKET}", "./", recursive=True)
